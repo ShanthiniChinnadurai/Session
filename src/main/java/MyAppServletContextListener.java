5772cee630cf2de15b3ad771d87331a1502a6761
Trainee <Trainee@KGFSL.COM>
2018-06-14 16:08:47 +0530

    first commit

---
+++ src/main/java/MyAppServletContextListener.java
@@ -0,0 +1,19 @@
+//package servletlisteners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+@WebListener
+public class MyAppServletContextListener implements ServletContextListener{
+    
+   @Override
+   public void contextDestroyed(ServletContextEvent arg0) {
+       System.out.println("ServletContextListener destroyed");
+   }
+
+       //Run this before web application is started
+   @Override
+   public void contextInitialized(ServletContextEvent arg0) {
+       System.out.println("ServletContextListener started");    
+   }
+}
